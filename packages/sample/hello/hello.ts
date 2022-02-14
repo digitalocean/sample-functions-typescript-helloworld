53111d5198c25d91f23720cc962e248388160934
programmersstudio <pulkitkhullar15@gmail.com>
2022-02-14 17:26:17 +0530

    initial commit

---
+++ packages/sample/hello/hello.ts
@@ -0,0 +1,7 @@
+export function main(args: {}): {} {
+    let name: string = args['name'] || 'stranger'
+    let greeting: string = 'Hello ' + name + '!'
+    console.log(greeting)
+    return { body: greeting }
+  }
+  
